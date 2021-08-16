7d5366750ea17a58bc51fe16aa122192aac8e138
loanto-ant <lorenz.filla@gmail.com>
2021-08-16 19:48:20 +0200

    added all files

---
+++ app.js
@@ -0,0 +1,15 @@
+function amogus() {
+    var audio = new Audio("Amogus.mp3");
+    audio.play();
+    document.getElementById("bild").src = "amogus.jpg";
+}
+function classicamongus() {
+    var audio = new Audio("Among Us.mp3");
+    audio.play();
+    document.getElementById("bild").src = "Classic Among Us.png";
+}
+function amongusdrip() {
+    var audio = new Audio("Among Us Drip.mp3");
+    audio.play();
+    document.getElementById("bild").src = "among us drip.jpg";
+}
